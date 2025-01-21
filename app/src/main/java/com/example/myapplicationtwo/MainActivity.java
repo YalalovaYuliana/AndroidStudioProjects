e4dce8c852e3179d3fc19b35083de890f4bc1869
Yalalova Yuliana <ulianaualalova.ranepa@gmail.com>
2025-01-21 22:30:30 +0200

    add different layouts

---
+++ app/src/main/java/com/example/myapplicationtwo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplicationtwo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
